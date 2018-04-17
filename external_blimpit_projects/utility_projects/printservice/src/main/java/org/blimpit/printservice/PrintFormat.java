dfb459934de99e5414f1abe2a3071e60bd32ed9a
isururanawaka <irjanith@gmail.com>
2018-04-17 12:14:52 +0530

    Add API methods

---
+++ external_blimpit_projects/utility_projects/printservice/src/main/java/org/blimpit/printservice/PrintFormat.java
@@ -0,0 +1,7 @@
+package org.blimpit.printservice;
+
+/**
+ *  Format of the printing document
+ */
+public interface PrintFormat {
+}
--- external_blimpit_projects/utility_projects/printservice/src/main/java/org/blimpit/printservice/PrintService.java
+++ external_blimpit_projects/utility_projects/printservice/src/main/java/org/blimpit/printservice/PrintService.java
@@ -1,9 +1,37 @@
 package org.blimpit.printservice;
 
 /**
- * This interface provides the  entry points for print service functions
+ *  print service functions
  */
 public interface PrintService {
 
+    /**
+     * Print the given string using given printer
+     * @param printableStr Formatted String to be printed
+     * @param printerName Name of the printer
+     * @return Indicate the success of the printer job
+     */
+    boolean print(String printableStr, String printerName);
+    /**
+     * Print using first available printer
+     * @param printableStr Formatted String to be printed
+     * @return Indicate the success of the printer job
+     */
+    boolean print(String printableStr);
+    /**
+     * Can provide the customized page formats for the printer
+     * @param printableStr  Formatted String to be printed
+     * @param format  customized parameters of the document to be printed
+     * @param printerName Name of the printer
+     * @return Indicate the success of the printer job
+     */
+    boolean print(String printableStr, PrintFormat format, String printerName);
+    /**
+     * Can provide the customized page format for the printer
+     * @param printableStr  Formatted String to be printed
+     * @param format  customized parameters of the document to be printed
+     * @return Indicate the success of the printer job
+     */
+    boolean print(String printableStr, PrintFormat format);
     
 }
